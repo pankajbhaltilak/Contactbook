7a14a5831b5ba159b5b2ab1d48a84c1a3da5c354
pankajbhaltilak <pankaj.bhaltilak.1@gmail.com>
2022-08-25 14:27:01 +0530

    first commit

---
+++ home/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Contact(models.Model):
+    FirstName = models.CharField(max_length=30)
+    LastName = models.CharField(max_length=30)
+    Email = models.EmailField()
+    ContactNumber = models.CharField(max_length=10)
